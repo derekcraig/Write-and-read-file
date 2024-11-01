231193a0038d818029546cce75d3a65b6905a705
derekcraig <124757939+derekcraig@users.noreply.github.com>
2024-11-01 18:11:56 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>   //Needed to read and write files
+using namespace std;
+
+int main()
+{
+ ofstream ofile;                  //Creates an out stream to ofile
+ ofile.open("Numbers.txt");       //Opens ofile and names it
+ int num, sum = 0;
+ for(num = 1; num <= 100; num++)  //Writes numbers 1-100 to a text file
+  {
+   ofile << num << endl;
+  }
+ ofile.close();                   //Closes file
+ 
+ cout << "Reading data from file.\n";
+ ifstream ifile;                  //Creates another stream to read
+ ifile.open("Numbers.txt");       //Opens ifile
+ while(ifile >> num)              //Reads all available lines
+  {
+   cout << num << endl;
+   sum += num;                    //Also sums them just for funsies
+  }
+ ifile.close();                   //Closes file
+ cout << "The sum of the numbers is :" << sum << endl;
+return 0;
+}
